211f38ec238415d98f5d01c8f67625e54a9eb1cb
Anthony DiBernardo <69294054+antiman9071@users.noreply.github.com>
2025-03-13 13:38:07 -0400

    Add files via upload

---
+++ LightChain.java
@@ -0,0 +1,136 @@
+import itsc2214.Node;
+/**
+ * A class that implements a chain of lights for
+ * the holidays. Each chain contains initially 5
+ * but you can remove some or change some by
+ * colors.
+ *
+ * TODO Change your EMAIL below
+ * @author  mperez@charlotte.edu
+ * TODO Add your team members' emails for group submissoin
+ * @author mspitz1@charlotte.edu
+ * @author wowen3@charlotte.edu
+ */
+
+class LightChain {
+
+    /**
+     * Use these constants in the main program to pass
+     * a color to the routines in this file. When you
+     * print this string, the output will set the 
+     * background of the appropriate color.
+     */
+
+    /** LightChain.RED. */
+    final static String RED    = "\u001b[41m Red  \u001b[0m";
+
+    /** LightChain.GREEN. */
+    final static String GREEN  = "\u001b[42mGreen \u001b[0m";
+
+    /** LightChain.YELLOW. */
+    final static String YELLOW = "\u001b[43mYellow\u001b[0m";
+
+    /** LightChain.BLUE. */
+    final static String BLUE   = "\u001b[44m Blue \u001b[0m";
+
+    /** LightChain.ORANGE. */
+    final static String ORANGE = "\u001b[41mOrange\u001b[0m";
+
+    /** LightChain.PURPLE. */
+    final static String PURPLE = "\u001b[45mPurple\u001b[0m";
+
+    /** LightChain.WHITE. */
+    final static String WHITE  = "White";
+
+    /**
+     * The reference to the beginning light node of this chain.
+     */
+    protected Node<String> headOfLightString;
+
+    /**
+     * Constructor, build a chain of lights: Red,Green,Blue,Yellow,White,
+     * initializing the instance variable, headOfLightString
+     */
+    public LightChain() {
+        // TODO Create a linked list of holiday lights as a chain of lights, 
+        //     using the following colors: Red, Green, Blue, Yellow, and White. 
+
+    }
+
+    /**
+     * Returns the color of the light bulb at
+     * position x.
+     * @param pos position of the bulb to get
+     * @return color
+     */
+    public String get(int pos) {
+        Node<String> cur = headOfLightString;
+        while (cur != null && pos > 0) {
+            pos--;
+            cur = cur.getNext();
+        }
+        if (cur != null) {
+            return cur.getData();
+        }
+        else {
+            return null;
+        }
+    }
+
+    /**
+     * Join two LightChains by connecting anotherPiece
+     * to the end of this one.
+     * 
+     * @param anotherPiece a LightChain to append to this one
+     * @return this chain, appropriate for doing multiple chaining
+     */
+    public LightChain join(LightChain anotherPiece) {
+        Node<String> cur = headOfLightString;
+        while (cur.getNext() != null) {
+            cur = cur.getNext();
+        }
+        cur.setNext(anotherPiece.headOfLightString);
+        return this;
+    }
+
+    /**
+     * Traverse the light chain, printing the node 
+     * value for each.
+     */
+    public void traverseLightChain() {
+        // TODO Traverse the chain of lights and print out. 
+        //     the color of each light in order.
+
+    }
+
+    /**
+     * Insert the newColor after another color (afterColor).
+     * @param newColor   the color to be inserted
+     * @param afterColor insert it after this color
+     */
+    public void insertAfter(String newColor, String afterColor) {
+        // TODO Add a new light (newColor) after lights. 
+
+    }
+
+    /**
+     * Remove the given color.
+     * @param aColor the to-be-removed color
+     */
+    public void remove(String aColor) {
+        //TODO Remove the a color from the chain.
+
+    }
+    
+    /**
+     * Replace all of the colors aColor with the given anotherColor.
+     * @param aColor the color to be replaced.
+     * @param anotherColor the color that will replace it.
+     */
+    public void replaceAll(String aColor, String anotherColor) {
+        //TODO Replace all lightbulbs of a color light with another light.
+
+    }
+
+}
+
